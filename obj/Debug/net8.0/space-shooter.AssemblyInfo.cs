4d1ede129b9164e55bdf83b859892c86a78d1e7e
Victor Ubere <victorubere@gmail.com>
2025-02-15 13:57:50 +0100

    run files

--- obj/Debug/net8.0/space-shooter.AssemblyInfo.cs
+++ obj/Debug/net8.0/space-shooter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("space-shooter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+433e0ce2c77f69fe25b1e023639702b4587372ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88abce315f74d93eae241cb412532e88164b9251")]
 [assembly: System.Reflection.AssemblyProductAttribute("space-shooter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("space-shooter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
